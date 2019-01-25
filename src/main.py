e9efa63da3c7de97b254112e1c133109a59528c6
Joel Klint <joel.klint@gmail.com>
2019-01-25 12:19:15 +0100

    can send email to multiple recipients

--- src/main.py
+++ src/main.py
@@ -2,25 +2,30 @@ import os
 import email_service
 import currency_service
 
-EMAIL_RECIPIENT=os.environ['CURRENCY_INSIGHT_EMAIL_RECIPIENT']
+EMAIL_RECIPIENTS=os.environ['CURRENCY_INSIGHT_EMAIL_RECIPIENT']
 
 if __name__ == '__main__':
-    today=currency_service.get_rate()
+    current=currency_service.get_rate_at_day()
     weekly=currency_service.get_weekly_change()
     monthly=currency_service.get_monthly_change()
-    email_text = email_service.prepare_email_txt(
-        today=today,
-        weekly=weekly,
-        monthly=monthly
-    )
+    monthly_plot = currency_service.create_monthly_plot()
+    yearly_plot = currency_service.create_yearly_plot()
+    # email_text = email_service.prepare_email_txt(
+    #     today=today,
+    #     weekly=weekly,
+    #     monthly=monthly
+    # )
     email_html = email_service.prepare_email_html(
-        today=today,
+        current=current,
         weekly=weekly,
-        monthly=monthly
+        monthly=monthly,
+        monthly_plot=monthly_plot,
+        yearly_plot=yearly_plot
     )
-    email_service.send_email(
-        to=EMAIL_RECIPIENT,
-        subject='DKK/SEK insight',
-        text=email_text,
-        html=email_html
-    )
+    for recipient in EMAIL_RECIPIENTS.split(','):
+        email_service.send_email(
+            to=recipient,
+            subject='DKK/SEK insight',
+            text='',
+            html=email_html
+        )
